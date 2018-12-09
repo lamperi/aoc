7e7762c74efd71b33698ad03b323a167c510e45b
Toni Fadjukoff <lamperi@gmail.com>
2018-12-09 14:01:46 +0200

    Rewrite solution with deque

--- 2018/09/solve.py
+++ 2018/09/solve.py
@@ -1,30 +1,25 @@
+from collections import deque
 
-def solve(players, last_marble):
-    marbles = [0]
-    index = 0
+def solve_deque(players, last_marble):
+    marbles = deque([0])
     scores = [0]*(1+players)
     for marble in range(1, last_marble+1):
         if marble % 23 == 0:
             p = marble%players
             scores[p] += marble
-            index -= 7
-            if index < 0:
-                index = len(marbles)+index
-            scores[p] += marbles.pop(index)
+            marbles.rotate(7)
+            scores[p] += marbles.popleft()
         else:
-            index += 2
-            if index > len(marbles):
-                index = 1
-            marbles.insert(index, marble)
+            marbles.rotate(-2)
+            marbles.appendleft(marble)
     return max(scores)
 
-print(solve(9, 25))
+print(solve_deque(9, 25))
+print(solve_deque(10, 1618), 8317)
+print(solve_deque(13, 7999), 146373)
+print(solve_deque(17, 1104), 2764)
+print(solve_deque(21, 6111), 54718)
+print(solve_deque(30, 5807), 37305)
 
-print(solve(10, 1618), 8317)
-print(solve(13, 7999), 146373)
-print(solve(17, 1104), 2764)
-print(solve(21, 6111), 54718)
-print(solve(30, 5807), 37305)
-
-print(solve(435, 71184))
-# Too slow for part 2 due to using non-linked-list
+print(solve_deque(435, 71184))
+print(solve_deque(435, 71184*100))
