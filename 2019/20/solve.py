0f5f080741476a899765828cde3526e720beff7c
Toni Fadjukoff <lamperi@gmail.com>
2019-12-20 06:45:18 +0100

    Fix a bug on Day 20 of 2019
    The BFS did not iterate all paths in the correct order: mainly it had and offset
    of 1 of every teleportation due to spending one too many steps teleportating
    making it possible to find the wrong optimal paths. Now the loop invariant
    always increases path length by one.

--- 2019/20/solve.py
+++ 2019/20/solve.py
@@ -23,40 +23,41 @@ def solve(data):
                 if area.get((y+1,x), "?") in string.ascii_uppercase:
                     portal = area[(y,x)] + area[(y+1,x)]
                     if area.get((y+2,x), "?") == ".":
-                        portals[(y+1,x)] = portal
+                        portals[(y+2,x)] = portal
                     else:
-                        portals[(y,x)] = portal
+                        portals[(y-1,x)] = portal
                 elif area.get((y,x+1), "?") in string.ascii_uppercase:
                     portal = area[(y,x)] + area[(y,x+1)]
                     if area.get((y,x+2), "?") == ".":
-                        portals[(y,x+1)] = portal
+                        portals[(y,x+2)] = portal
                     else:
-                        portals[(y,x)] = portal
+                        portals[(y,x-1)] = portal
     
     for coord, portal in portals.items():
         if portal == "AA":
             start = coord
         elif portal == "ZZ":
             end = coord
-    visited = {start: -1}
+    del portals[start]
+    del portals[end]
+    visited = {start: 0}
     l = [start]
     while l:
         pos = l[0]
         l = l[1:]
         for new_pos in (pos_sum(pos, dir_to_pos(i)) for i in (1,2,3,4)):
             if new_pos == end:
-                return visited[pos]
+                return visited[pos] + 1
             elif new_pos in visited:
                 continue
             elif area.get(new_pos, "") == ".":
                 l.append(new_pos)
                 visited[new_pos] = visited[pos] + 1
-            elif area.get(new_pos, "?") in string.ascii_uppercase and area[pos] == ".":
-                portal = portals[new_pos]
-                #print(portal, new_pos, portals)
-                pair_portal_pos = [pp for pp, p in portals.items() if p == portal and pp != new_pos][0]
+            elif area.get(new_pos, "?") in string.ascii_uppercase and pos in portals:
+                portal = portals[pos]
+                pair_portal_pos = [pp for pp, p in portals.items() if p == portal and pp != pos][0]
                 l.append(pair_portal_pos)
-                visited[pair_portal_pos] = visited[pos]
+                visited[pair_portal_pos] = visited[pos] + 1
                 
 def solve2(data):
     area = {}
@@ -72,51 +73,52 @@ def solve2(data):
                 if area.get((y+1,x), "?") in string.ascii_uppercase:
                     portal = area[(y,x)] + area[(y+1,x)]
                     if area.get((y+2,x), "?") == ".":
-                        portals[(y+1,x)] = portal
+                        portals[(y+2,x)] = portal
                     else:
-                        portals[(y,x)] = portal
+                        portals[(y-1,x)] = portal
                 elif area.get((y,x+1), "?") in string.ascii_uppercase:
                     portal = area[(y,x)] + area[(y,x+1)]
                     if area.get((y,x+2), "?") == ".":
-                        portals[(y,x+1)] = portal
+                        portals[(y,x+2)] = portal
                     else:
-                        portals[(y,x)] = portal
+                        portals[(y,x-1)] = portal
     
     for coord, portal in portals.items():
         if portal == "AA":
             start = coord
         elif portal == "ZZ":
             end = coord
-    visited = {(0,start): -1}
+    # These are not actual portals!
+    del portals[start]
+    del portals[end]
+    visited = {(0,start): 0}
     l = [(0,start)]
     while l:
         level, pos = state = l[0]
         l = l[1:]
         for new_pos in (pos_sum(pos, dir_to_pos(i)) for i in (1,2,3,4)):
             if new_pos == end and level == 0:
-                return visited[(level,pos)]
+                return visited[(level,pos)] + 1
+            elif pos in portals and area.get(new_pos, "?") in string.ascii_uppercase:
+                if new_pos[0] < 5 or (max_y-new_pos[0]) < 5 or new_pos[1] < 5 or (max_x-new_pos[1]) < 5:
+                    new_level = level-1
+                else:
+                    new_level = level+1
+                if new_level < 0: # Dead path
+                    continue
+                portal = portals[pos]
+                pair_portal_pos = [pp for pp, p in portals.items() if p == portal and pp != pos][0]
+                new_state = (new_level, pair_portal_pos)
+                if new_state in visited:
+                    continue
+                l.append(new_state)
+                visited[new_state] = visited[state] + 1
             elif area.get(new_pos, "") == ".":
                 new_state = (level, new_pos)
                 if new_state in visited:
                     continue
                 l.append(new_state)
                 visited[new_state] = visited[state] + 1
-            elif area.get(new_pos, "?") in string.ascii_uppercase and area[pos] == ".":
-                if new_pos == start or new_pos == end:
-                    continue
-                if new_pos[0] < 5 or (max_y-new_pos[0]) < 5 or new_pos[1] < 5 or (max_x-new_pos[1]) < 5:
-                    new_level = level-1
-                else:
-                    new_level = level+1
-                if new_level < 0:
-                    continue
-                portal = portals[new_pos]
-                pair_portal_pos = [pp for pp, p in portals.items() if p == portal and pp != new_pos][0]
-                new_state = (new_level, pair_portal_pos)
-                l.append(new_state)
-                visited[new_state] = visited[state]
-
-
 
 print(solve("""                   A               
                    A               
