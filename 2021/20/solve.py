0623a1c0ef713d7d7719a5faeb0a0ed2d0b1ed2e
Toni Fadjukoff <lamperi@gmail.com>
2021-12-21 05:53:10 +0100

    Small refactorings to day 20 of 2021

--- 2021/20/solve.py
+++ 2021/20/solve.py
@@ -1,13 +1,4 @@
-import operator
 import os.path
-import re
-from collections import Counter, defaultdict
-from functools import partial, reduce
-from heapq import heappop, heappush
-from io import TextIOBase
-from copy import deepcopy
-from itertools import combinations, pairwise, permutations
-from math import isfinite, prod
 
 INPUT=os.path.join(os.path.dirname(__file__), "input.txt")
 with open(INPUT) as f:
@@ -23,35 +14,48 @@ test="""..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..
 """
 
 def get_window(image, y, x, d):
-    for yd in (-1, 0, 1):
-        for xd in (-1, 0, 1):
-            if 0 <= y+yd < len(image) and 0 <= x+xd < len(image[y+yd]):
-                yield image[y+yd][x+xd]
-            else:
-                yield d
+    for yd in (-2, -1, 0):
+        for xd in (-2, -1, 0):
+            yield image.get((y+yd, x+xd), d)
+
+def get_new_pixel(image, y, x, d, enchancement):
+    index = 0
+    for i, b in enumerate(get_window(image, y, x, d)):
+        index |= (b << (8-i))
+    return enchancement[index]
+
+def print_map(image, d):
+    max_y = max(y for y,_ in image.keys())
+    max_x = max(x for _,x in image.keys())
+    s = ""
+    for y in range(0, max_y):
+        for x in range(0, max_x):
+            s += "#" if image.get((y,x), d) == 1 else "."
+        s+="\n"
+    print(s)
+
 
 def get_lit_pixels(data, t):
     enchancement, image = data.split("\n\n")
-    # Make image mutable
-    image = [[c for c in line] for line in image.splitlines()]
-    d = "."
+    enchancement = [1 if c == "#" else 0 for c in enchancement]
+    image = {(y,x): 1 if c == "#" else 0
+            for y,line in enumerate(image.splitlines())
+            for x, c in enumerate(line)}
+    d = 0
+    max_y = max(y for y,_ in image.keys()) + 3
+    max_x = max(x for _,x in image.keys()) + 3
     for _ in range(t):
-        # pad image by one pixel because the window is 3x3
-        # fill with the appropriate default pixel for the iteration
-        image = [[d]*(len(image[0])+2)] + [[d] + line + [d] for line in image] + [[d]*(len(image[0])+2)]
-        newimage = deepcopy(image)
-        for y in range(0, len(newimage)):
-            for x in range(0, len(newimage[y])):
-                binary = ["1" if c == "#" else "0" for c in get_window(image, y, x, d)]
-                index = int("".join(binary), 2)
-                newimage[y][x] = enchancement[index]
-        image = newimage
+        image = {(y,x): get_new_pixel(image, y, x, d, enchancement)
+            for y in range(0, max_y)
+            for x in range(0, max_x)}
+        max_y += 2
+        max_x += 2
         # Note: depending on what the 0 value enchancement resolves, we need to 
         # change the default fill value of the infinite image.
-        if enchancement[0] == "#":
-            d = "." if d == "#" else "#"
+        if enchancement[0] == 1:
+            d = 1-d
 
-    return "".join("".join(l) for l in image).count("#")
+    return sum(image.values())
 
 def part1(data):
     return get_lit_pixels(data, t=2)
