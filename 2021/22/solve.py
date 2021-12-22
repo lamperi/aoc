7840594564a4a25f1402382a7713cdd14fedbe13
Toni Fadjukoff <lamperi@gmail.com>
2021-12-22 17:25:39 +0100

    Further simplify dividing areas, plus remove mentions to cubes

--- 2021/22/solve.py
+++ 2021/22/solve.py
@@ -55,71 +55,52 @@ def axis_intersection(min_x, max_x, min_x2, max_x2):
             or max_x2 >= min_x and min_x2 <= min_x)
 
 
-def intersection(cube1, cube2):
-    (min_x, max_x), (min_y, max_y), (min_z, max_z) = cube1
-    (min_x2, max_x2), (min_y2, max_y2), (min_z2, max_z2) = cube2
+def intersection(rectangle1, rectangle2):
+    (min_x, max_x), (min_y, max_y), (min_z, max_z) = rectangle1
+    (min_x2, max_x2), (min_y2, max_y2), (min_z2, max_z2) = rectangle2
     return (axis_intersection(min_x, max_x, min_x2, max_x2)
             and axis_intersection(min_y, max_y, min_y2, max_y2)
             and axis_intersection(min_z, max_z, min_z2, max_z2))
 
 
-def handle_axis(cube1, cube2, ax):
+def handle_axis(rectangle1, rectangle2, ax):
     shapes = []
-    min_a1, max_a1 = cube1[ax]
-    min_a2, max_a2 = cube2[ax]
-    if min_a1 <= min_a2 and max_a1 >= min_a2:
-        if max_a1 < max_a2:
-            shapes.append(cube2[:ax] + ((max_a1+1, max_a2),) + cube2[ax+1:])
-            max_a2 = max_a1
-    if min_a2 < min_a1 and max_a2 >= min_a1:
-        shapes.append(cube2[:ax] + ((min_a2, min_a1-1),) + cube2[ax+1:])
+    min_a1, max_a1 = rectangle1[ax]
+    min_a2, max_a2 = rectangle2[ax]
+    # case 1: cut the part where max_a2 > max_a1
+    if max_a1 < max_a2:
+        shapes.append(rectangle2[:ax] + ((max_a1+1, max_a2),) + rectangle2[ax+1:])
+        max_a2 = max_a1
+    # case 2: cut the part where min_a2 < min_a1
+    if min_a2 < min_a1:
+        shapes.append(rectangle2[:ax] + ((min_a2, min_a1-1),) + rectangle2[ax+1:])
         min_a2 = min_a1
-    if max_a2 >= max_a1 and min_a2 <= max_a1:
-        if max_a1+1 <= max_a2:
-            shapes.append(cube2[:ax] + ((max_a1+1, max_a2),) + cube2[ax+1:])
-            max_a2 = max_a1
-    return cube2[:ax] + ((min_a2, max_a2),) + cube2[ax+1:], shapes
+    # update the axis range so we can use it for next axis
+    return rectangle2[:ax] + ((min_a2, max_a2),) + rectangle2[ax+1:], shapes
 
 
-def divide_rectangle_into_non_intersecting_shapes(cube1, cube2, test=False):
+def divide_rectangle_into_non_intersecting_shapes(rectangle1, rectangle2):
     shapes = []
-    cube2_, s = handle_axis(cube1, cube2, 0)
+    rectangle2, s = handle_axis(rectangle1, rectangle2, 0)
     shapes.extend(s)
-    cube2_, s = handle_axis(cube1, cube2_, 1)
+    rectangle2, s = handle_axis(rectangle1, rectangle2, 1)
     shapes.extend(s)
-    cube2_, s = handle_axis(cube1, cube2_, 2)
+    _, s = handle_axis(rectangle1, rectangle2, 2)
     shapes.extend(s)
-    if test:
-        assert intersection(cube1, cube2)
-        for s1, s2 in combinations(shapes, 2):
-            assert not intersection(s1, s2), f'{s1} and {s2}'
-        for s in shapes:
-            assert not intersection(cube1, s), f'{cube1} and {s}'
-            for ax in s:
-                assert ax[1] >= ax[0], f'{s}'
-        a1 = get_init_area([cube1, cube2])
-        a2 = get_init_area(shapes + [cube1])
-        if a1 != a2:
-            print('compare', count_init_area(
-                shapes + [cube1]), count_init_area([cube1, cube2]))
-            print('missing', list(a1 - a2)[:10])
-            print('extra', list(a2 - a1)[:10])
-        assert count_init_area(shapes + [cube1]) == count_init_area(
-            [cube1, cube2]), f'failed for {cube1} and {cube2}: {shapes}'
     return shapes
 
 
-def get_pixels(cubes):
+def get_pixels(rectangles):
     pixels_on = 0
-    for (min_x, max_x), (min_y, max_y), (min_z, max_z) in cubes:
+    for (min_x, max_x), (min_y, max_y), (min_z, max_z) in rectangles:
         pixels_on += abs(1 + max_x - min_x) * \
             abs(1 + max_y - min_y) * abs(1 + max_z - min_z)
     return pixels_on
 
 
-def get_init_area(cubes):
+def get_init_area(rectangles):
     on = set()
-    for (min_x, max_x), (min_y, max_y), (min_z, max_z) in cubes:
+    for (min_x, max_x), (min_y, max_y), (min_z, max_z) in rectangles:
         min_x = max(-50, min_x)
         max_x = min(50, max_x)
         min_y = max(-50, min_y)
@@ -133,8 +114,8 @@ def get_init_area(cubes):
     return on
 
 
-def count_init_area(cubes):
-    return len(get_init_area(cubes))
+def count_init_area(rectangles):
+    return len(get_init_area(rectangles))
 
 
 def part2(data):
@@ -159,9 +140,9 @@ def part2(data):
                 if cmd == 'on':
                     new_shapes = divide_rectangle_into_non_intersecting_shapes(
                         shape, current)
-                    # Get shapes that don't intersect with this cube.
+                    # Get shapes that don't intersect with this rectangle.
                     # Push into beginning of inputs because order matters.
-                    # Save index so we don't check already visited cubes again (Expensive)
+                    # Save index so we don't check already visited rectangles again (Expensive)
                     next_inputs = [('on', rectangle, index+1)
                                    for rectangle in new_shapes]
                     inputs.extendleft(next_inputs)
@@ -172,7 +153,7 @@ def part2(data):
                     to_add.extend(new_shapes)
                     to_remove.append(shape)
         else:
-            # Only add the cube if we didn't have to split it.
+            # Only add the rectangle if we didn't have to split it.
             # With break
             if cmd == 'on':
                 non_intersecting_shapes.append(current)
@@ -188,20 +169,6 @@ def part2(data):
 
 
 if self_test:
-    assert not axis_intersection(10, 10, 11, 12)
-    assert not axis_intersection(11, 12, 10, 10)
-    assert axis_intersection(10, 11, 11, 12)
-    assert axis_intersection(10, 12, 5, 10)
-    divide_rectangle_into_non_intersecting_shapes(((-20, 26), (-36, 17), (-47, 7)),
-                                                  ((-22, 28), (-29, 23), (-38, 16)), test=True)
-    divide_rectangle_into_non_intersecting_shapes(((27, 33), (-21, 23), (-26, 28)),
-                                                  ((27, 28), (-29, 23), (-38, 16)), test=True)
-    divide_rectangle_into_non_intersecting_shapes(((-20, 7), (24, 46), (-50, -1)),
-                                                  ((-20, 23), (24, 26), (-21, 29)), test=True)
-    divide_rectangle_into_non_intersecting_shapes(((-48, -32), (26, 41), (-47, -37)),
-                                                  ((-46, -23), (-6, 46), (-50, -1)), test=True)
-    divide_rectangle_into_non_intersecting_shapes(((-15, -4), (-14, 5), (-24, -11)),
-                                                  ((-9, -4), (-11, 36), (-45, 2)), test=True)
     assert(part2(test) == (39, 39))
     assert(part2(test2)[1] == 590784)
     assert(part2(test3) == (2758514936282235, 474140))
