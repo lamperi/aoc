cc1852d12b339e7bff87e78fd82e0196f61089ff
Toni Fadjukoff <lamperi@gmail.com>
2022-12-01 06:13:43 +0100

    Day 1 of 2022

---
+++ 2022/1/solve.py
@@ -0,0 +1,29 @@
+import os.path
+
+INPUT = os.path.join(os.path.dirname(__file__), 'input.txt')
+with open(INPUT) as f:
+    data = f.read()
+
+TEST = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+
+def solve(data):
+    elves = data.split("\n\n")
+    calories = [sum(map(int, elf.splitlines())) for elf in elves]
+    calories.sort()
+    return calories[-1], sum(calories[-3:])
+
+print(solve(TEST))
+print(solve(data))
