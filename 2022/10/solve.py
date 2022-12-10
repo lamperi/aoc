743be6b9b6e021f2120f50a60a61d3418bce55d3
Toni Fadjukoff <lamperi@gmail.com>
2022-12-10 06:27:36 +0100

    Day 10 of 2022

---
+++ 2022/10/solve.py
@@ -0,0 +1,185 @@
+import os.path
+
+FILE = os.path.join(os.path.dirname(__file__), 'input.txt')
+with open(FILE) as f:
+    INPUT = f.read()
+
+TEST = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"""
+
+def solve(data):
+    cycle = []
+    X = 1
+    for line in data.splitlines():
+        match line.split():
+            case ("addx", V):
+                cycle.append(X)
+                cycle.append(X)
+                X += int(V)
+            case ("noop",):
+                cycle.append(X)
+            case other:
+                assert False, line
+    s = 0
+    for i in range(20, len(cycle), 40):
+        s += cycle[i-1]*i
+
+    o = []
+    L = 40
+    for i,x in enumerate(cycle):
+        if i%L == 0:
+            print("".join(o))
+            o = []
+        if abs(i%L - x) <= 1:
+            o.append("#")
+        else:
+            o.append(".")
+        
+    return s
+
+print(solve(TEST))
+print(solve(INPUT))
