a53fde42db359dae0e490fb43fa2109328897c90
Toni Fadjukoff <lamperi@gmail.com>
2023-12-04 05:57:01 +0100

    Python template to copy for next day

---
+++ template.py
@@ -0,0 +1,21 @@
+import os.path
+
+INPUT = os.path.join(os.path.dirname(__file__), 'input.txt')
+with open(INPUT) as f:
+    data = f.read()
+
+def part1(data):
+    return
+
+test = """"""
+print(part1(test))
+print(part1(data))
+
+def part2(data):
+    return
+
+# Override test for part 2.
+# test = """ """
+
+print(part2(test))
+print(part2(data))
