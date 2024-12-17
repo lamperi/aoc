f21fc53e4a67541b9681cc6caf581ba9ef25b20f
Toni Fadjukoff <lamperi@gmail.com>
2024-12-17 23:35:47 +0100

    Day 17 of 2024 in Python; reworked the solution to a quicker and cleaner one by getting inspired by the discussion for the daily problem

--- 2024/17/solve.py
+++ 2024/17/solve.py
@@ -72,39 +72,24 @@ Program: 0,1,5,4,3,0"""
 assert "4,6,3,5,6,3,5,2,1,0" == part1(test)
 print(part1(data))
 
-def part2_pre_process(data):
-    registers,program = data.split("\n\n")
-    reg = {}
-    for register in registers.splitlines():
-        _, name, value = register.split()
-        reg[name[:-1]] = int(value)
-    program = [int(inst) for inst in program.split()[1].split(",")]
+def part2_search(data):
+    _registers, program = data.split("\n\n")
+    program = tuple(int(inst) for inst in program.split()[1].split(","))
     
     reg = {'A': 117440, 'B': 0, 'C': 0}
-    out = run_program(reg, program)
-    return out == program
+    
+    stack = [(1, 0)]
+    all_found = []
+    while stack:
+        n, a = stack.pop()
+        for next_three_bits in range(1 << 3):
+            attempt = (a << 3) | next_three_bits
+            out = run_program(reg|dict(A=attempt), program)
+            if out[0] == program[-n]:
+                if len(out) == len(program):
+                    all_found.append(attempt)
+                else:
+                    stack.append((n+1, attempt))
+    return min(all_found)
 
-def part2(data):
-    _registers, program = data.split("\n\n")
-    program = [int(inst) for inst in program.split()[1].split(",")]
-    want = tuple(program)
-    # A must be between 35184372088832 (1 << 45) inclusive and 281474976710656 exclusive (1 << 48)
-    # Through brute forcing numbers that have many correct digits in the output:
-    # the following bit ranges are always the same:
-    # I believe this is specific to my input and doesn't apply to others.
-    BITS_1_TO_16  = 0b0011110000001111
-    BITS_33_TO_46 = 0b10110101011101
-    # So let's brute force the rest of the ranges.
-    candidates = []
-    for bits_17_to_32 in range(1<<16):
-        for bits_47_to_48 in range(1<<2):
-            v = combine(bits_47_to_48, BITS_33_TO_46, bits_17_to_32, BITS_1_TO_16)
-            p = run_program({'A': v, 'B': 0, 'C': 0}, program)
-            if tuple(p) == want:
-                candidates.append(v)
-    return min(candidates)
-
-def combine(u, x, y, z):
-    return (u << 14 << 16 << 16) | (x << 16 << 16) | (y << 16) | (z)
-
-print(part2(data))
+print(part2_search(data))
